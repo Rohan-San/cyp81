2959e23f72b9f013ab20bb92f1a52b12f3be3f4f
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2025-03-15 08:38:20 +0530

    Create server.js

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
